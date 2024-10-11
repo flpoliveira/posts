4ccf2f22a21825a17fac7450f95e9b6acc1f5b71
Filipe Oliveira <filipedasilvadeoliveira@gmail.com>
2024-10-11 00:24:47 -0300

    feat: adding feed

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -6,7 +6,7 @@ function Post() {
       <span className="text-primary text-md font-semibold">
         Sunday, April 3d 2024
       </span>
-      <span className="text-foregroundSecondary font-normal">
+      <span className="text-foregroundSecondary text-md font-normal">
         Cooking delicious and nutritious meals on weeknights can often feel like
         a daunting task. However, with a few simple strategies, it’s possible to
         whip up satisfying dinners in no time. One key is to plan ahead and keep
@@ -48,7 +48,7 @@ function Feed() {
             Found 6 posts of Cooking
           </span>
         </div>
-        <div className="px-4 flex flex-col gap-0 [&:not(:last-child)]:border-b">
+        <div className="px-4 flex flex-col gap-0 [&>div:not(:last-child)]:border-b">
           <Post />
           <Post />
         </div>
